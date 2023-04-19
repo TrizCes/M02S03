88e038334d723cbeb75c5c581d489aadffb58362
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-18 22:29:34 -0300

    Instâncias para a classe Proprietario
    3 Instâncias da classe proprietário, em três variáveis  proprietario1, proprietario2, proprietario3.

--- CadastroDeCarros/Program.cs
+++ CadastroDeCarros/Program.cs
@@ -3,6 +3,32 @@ internal class Program
 {
   private static void Main(string[] args)
   {
+    Proprietario proprietario1 = new Proprietario()
+    {
+      nome = "Beatriz",
+      cpf = "987.753.159-64",
+      telefone = "(47) 96547-7895",
+      endereco = "Rua Bela Vista, nº 821"
+    };
+    proprietario1.CadastrarProprietario();
+
+    Proprietario proprietario2 = new Proprietario()
+    {
+      nome = "Vopen",
+      cpf = "673.660.120-76",
+      telefone = "(47) 96547-7895",
+      endereco = "Rua Três Barras, nº 377"
+    };
+    proprietario2.CadastrarProprietario();
+
+    Proprietario proprietario3 = new Proprietario()
+    {
+      nome = "Melra",
+      cpf = "962.075.940-02",
+      telefone = "(47) 96547-7895",
+      endereco = "Rua Professor José Motta, nº 99"
+    };
+    proprietario3.CadastrarProprietario();
 
   }
 }
