318b4f9ea699be1b97c3b159d83253d82576b087
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-18 21:32:36 -0300

    Método Soma de números
    Um método que receba por parâmetro dois números inteiros e retorna a soma deles.

---
+++ SomaDeNumeros/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+
+    int Calcula(int num1, int num2)
+    {
+      return num1 + num2;
+    }
+
+    Console.WriteLine(Calcula(3, 5));
+  }
+}
+
+
