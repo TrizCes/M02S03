89d7419579bde26b8e6f5ff4164cda5d420df2ce
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-18 21:51:17 -0300

    Sobrecargas do método Soma de números
    Duas sobrecargas do método Soma de números, criado no exercício 1.
    
    A primeira recebendo dois valores do tipo Double e retornando a soma deles.
    
    E a segunda recebendo 3 Parâmetros do Tipo double e retornando a soma deles

--- SomaDeNumeros/Program.cs
+++ SomaDeNumeros/Program.cs
@@ -2,13 +2,24 @@
 {
   private static void Main(string[] args)
   {
-
-    int SomaDeNumeros(int num1, int num2)
-    {
-      return num1 + num2;
-    }
-
+    Console.WriteLine(SomaDeNumeros(3.8, 5));
     Console.WriteLine(SomaDeNumeros(3, 5));
+    Console.WriteLine(SomaDeNumeros(3.8, 5.6, 7));
+  }
+
+  private static int SomaDeNumeros(int num1, int num2)
+  {
+    return num1 + num2;
+  }
+
+  private static double SomaDeNumeros(double num1, double num2)
+  {
+    return num1 + num2;
+  }
+
+  private static double SomaDeNumeros(double num1, double num2, double num3)
+  {
+    return num1 + num2;
   }
 }
 
