471af4c9065e5ce34b0584aadf7b3d7cd30c7f71
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-18 22:37:14 -0300

    Classe Carro
    uma classe Carro que contém a abstração de carro, a classe deve contém no mínimo os seguintes atributos:
    
    Marca
    
    Modelo
    
    Placa
    
    Cor
    
    Proprietário

---
+++ CadastroDeCarros/Carro.cs
@@ -0,0 +1,12 @@
+namespace CadastroDeCarros
+{
+  public class Carro
+  {
+    public string marca { get; set; }
+    public string modelo { get; set; }
+    public string placa { get; set; }
+    public string cor { get; set; }
+    public string renavam { get; set; }
+    public Proprietario proprietario { get; set; }
+  }
+}
